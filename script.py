2d06ea9915d4f78b32853d0d026f26582888855f
ninja8bpyt <78526646+ninja8bpyt@users.noreply.github.com>
2021-03-08 09:40:07 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,13 +1,13 @@
 class script(object):
-    START_TEXT = """ A Simple File Renamer Bot With Permanent Thumbnail support!💯
+    START_TEXT = """ I Am A Simple File Renamer Bot With Permanent Thumbnail support!💯
 
 <b>Send me any Telegram file and choose appropriate option! </b>"""
 
     RENAME_403_ERR = "What Are You Doing? You are Banned"
-    UPGRADE_TEXT = "CONTACT @prgofficial"
+    UPGRADE_TEXT = "CONTACT @Mr_Ninjas_Bot"
     DOWNLOAD_START = "Give Me Some Time..."
     UPLOAD_START = "Starting to upload..."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @prgofficial **"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @Bots_Ki_Duniya **"
     SAVED_THUMB = "Thumbnail Saved ✅ This Is Permanent"
     DEL_THUMB = "Thumbnail cleared succesfully!"
     NO_THUMB = "No thumbnails found!"
@@ -16,5 +16,6 @@ class script(object):
     HELP_USER = """It's not that complicated😅
     
 1. Send me any Telegram File.
-2. Choose appropriate option."""
+2. Choose appropriate option.
+3. Join @Bots_Ki_Duniya"""
 
